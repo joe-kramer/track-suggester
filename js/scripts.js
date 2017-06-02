1f8cb1f24d54ef78d434dada8f15b7080808a452
Joe Kramer <joekramer1125@gmail.com>
2017-06-02 10:25:12 -0700

    put first question in, wrapped in div to later make smooth transition between questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,31 @@
+//FRONT END
 $(document).ready(function() {
+
   $("#formOne").submit(function(event) {
     event.preventDefault();
     var name = $("input#name").val();
-
     $(".introPage").fadeOut(1000);
     $(".questionnaire").delay(1000).fadeIn(1000);
   });
+
+  $("formTwo").submit(function(event) {
+    event.preventDefault();
+    var q1 = parseInt($("input:radio[name=q1]:checked").val());
+
+  });
+
 });
+
+//BACK END
+var java = 0;
+var cSharp = 0;
+var php = 0;
+var ruby = 0;
+
+if(q1 === 1) {
+  cSharp += 1;
+  java += 1;
+} else if (q1 === 2) {
+  php += 1;
+  ruby += 1;
+}

851b5557aed1f9394ead05ca3e2a473d0950203e
Joe Kramer <joekramer1125@gmail.com>
2017-06-02 11:10:47 -0700

    complete question 2, transition between q1 and q2 working well

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,18 @@ $(document).ready(function() {
     $(".questionnaire").delay(1000).fadeIn(1000);
   });
 
-  $("formTwo").submit(function(event) {
+  $("#formTwo").submit(function(event) {
     event.preventDefault();
     var q1 = parseInt($("input:radio[name=q1]:checked").val());
+    question1(q1);
+    $(".q1Page").slideUp(500);
+    $(".q2Page").delay(500).slideDown(500);
+  });
 
+  $("#formThree").submit(function(event) {
+    event.preventDefault();
+    var q2 = parseInt($("input:radio[name=q2]:checked").val());
+    question2(q2);
   });
 
 });
@@ -22,10 +30,26 @@ var cSharp = 0;
 var php = 0;
 var ruby = 0;
 
-if(q1 === 1) {
-  cSharp += 1;
-  java += 1;
-} else if (q1 === 2) {
-  php += 1;
-  ruby += 1;
+//Q1
+var question1 = function(q1) {
+  if (q1 === 1) {
+    cSharp += 1;
+    java += 1;
+  } else if (q1 === 2) {
+    php += 1;
+    ruby += 1;
+  }
+}
+
+//Q2
+var question2 = function(q2) {
+  if (q2 === 1) {
+    ruby += 1;
+  } else if (q2 === 2) {
+    php += 1;
+  } else if (q2 === 3) {
+    cSharp += 1;
+  } else if (q2 === 4) {
+    java += 1;
+  }
 }
